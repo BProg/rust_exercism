b00822bdd761fbf366b2937da2558ee89f122a70
Ion Ostafi <ion.ostafi@ellation.com>
2020-01-18 22:34:53 +0200

    Make find function to be generic

--- binary-search/src/lib.rs
+++ binary-search/src/lib.rs
@@ -1,11 +1,15 @@
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
+pub fn find<T>(iter: impl AsRef<[T]>, key: T) -> Option<usize>
+where
+    T: PartialOrd + Copy + Clone + ?Sized,
+{
     let mut left = 0i32;
+    let array: Vec<T> = Vec::from(iter.as_ref());
     let mut right = array.len() as i32 - 1;
     while left <= right {
         let mid = (left + right) / 2;
         let value = array[mid as usize];
         if value == key {
-            return Some(mid as usize)
+            return Some(mid as usize);
         } else if value < key {
             left = mid + 1;
         } else if value > key {
