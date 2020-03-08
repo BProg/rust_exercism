af2cbfc895180b256c56d43512ed1dc0fca35b63
Ion Ostafi <ion.ostafi@ellation.com>
2020-03-08 08:33:33 +0200

    Refactor atbash cipher algorithm

--- atbash-cipher/src/lib.rs
+++ atbash-cipher/src/lib.rs
@@ -3,29 +3,30 @@ const SUBSTITUTION: &'static str = "zyxwvutsrqponmlkjihgfedcba1234567890";
 
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    plain.chars().fold(String::from(""), |mut encoded, ch| {
-        if let Some(pos) = ORIGINAL
-            .chars()
-            .position(|original_ch| original_ch == ch.to_ascii_lowercase())
-        {
-            if (encoded.len() + 1) % 6 == 0 {
-                encoded.push(' ');
-            }
-            encoded.push(SUBSTITUTION.chars().nth(pos).unwrap_or(ch));
-        }
-        encoded
+    replace(plain, ORIGINAL, SUBSTITUTION, |position| {
+        if (position + 1) % 6 == 0 { Some(' ') } else { None }
     })
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    cipher.chars().fold(String::from(""), |mut decoded, ch| {
-        if let Some(pos) = SUBSTITUTION
+    replace(cipher, SUBSTITUTION, ORIGINAL, |_| None)
+}
+
+fn replace<F>(input: &str, from: &str, to: &str, inject: F) -> String
+where
+    F: Fn(usize) -> Option<char>,
+{
+    input.chars().fold(String::from(""), |mut replaced, ch| {
+        if let Some(pos) = from
             .chars()
             .position(|subs_ch| subs_ch == ch.to_ascii_lowercase())
         {
-            decoded.push(ORIGINAL.chars().nth(pos).unwrap_or(ch));
+            if let Some(inject_char) = inject(replaced.len()) {
+                replaced.push(inject_char);
+            }
+            replaced.push(to.chars().nth(pos).unwrap_or(ch));
         }
-        decoded
+        replaced
     })
 }
